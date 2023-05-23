7393cdebef99984e63578b5ea7362b3eaa5b046b
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-05-23 08:40:40 +0000

    Created Basic Component for books

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+  import React from 'react';
 
-function Books() {
-  return (
-    <div>Books</div>
-  )
-}
+  function Books() {
+    return (
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 1
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 2
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 3
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 4
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 5
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 6
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 7
+        </div>
+        <div className="p-4 bg-blue-500 hover:bg-green-500 transition-colors duration-300">
+          Sem 8
+        </div>
+      </div>
+    );
+  }
 
-export default Books
+  export default Books;
